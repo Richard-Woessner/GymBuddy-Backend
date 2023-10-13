a63918339f301c5e73d3ee468b8c17f2050d4a70
ricky <richardwoesiii@gmail.com>
2023-10-13 12:32:34 -0400

    fixed dev script, added test route with firebase

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,40 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import usersRouter from './v1/controllers/UserController';
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import usersRouter from './v1/controllers/UserController'
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "../firebaseSecret";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { firebaseConfig } from '../firebaseSecret'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseApp)
 
-console.log(db);
+dotenv.config()
 
-
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT;
-
-
+const app: Express = express()
+const port = process.env.PORT
 
 // parses incoming requests with JSON payloads
-app.use(express.json());
+app.use(express.json())
 
-app.use(cors({ origin: ["http://localhost:8081", "https://origin2.com"] }));
+app.use(cors({ origin: ['http://localhost:8081', 'https://origin2.com'] }))
 
 /**
  * Routes.
  */
-app.use('/api', usersRouter);
+app.use('/api', usersRouter(db))
+
+console.log('test')
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-
+    res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
--- src/v1/controllers/UserController.ts
+++ src/v1/controllers/UserController.ts
@@ -1,22 +1,41 @@
 import { Router } from 'express'
+import { Firestore, collection, getDocs } from 'firebase/firestore'
 
-const router = Router()
+const UserWrapper = (db: Firestore) => {
+    const userRef = collection(db, 'users')
 
-router
-    .route('/users')
-    // to create new resources
-    .post((req, res, next) => {
-        res.send('POST request to the homepage')
-    })
-    // to retrieve resource
-    .get((req, res, next) => {
-        res.send('GET request to the homepage')
-    })
-router
-    .route('/users/:userId')
-    // to retrieve a single resource
-    .get((req, res, next) => {
-        res.send('GET request to the homepage')
-    })
+    const router = Router()
 
-export default router
+    router
+        .route('/users')
+        // to create new resources
+        .post((req, res, next) => {
+            res.send('POST request to the homepage')
+        })
+        // to retrieve resource
+        .get(async (req, res, next) => {
+            console.log('get users')
+
+            const querySnapshot = await getDocs(collection(db, 'Users'))
+
+            let users: any[] = []
+
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, ' => ', doc.data())
+                users.push(doc.data())
+            })
+
+            res.send(users)
+        })
+    router
+        .route('/users/:userId')
+        // to retrieve a single resource
+        .get((req, res, next) => {
+            res.send('GET request to the homepage')
+        })
+
+    return router
+}
+
+export default UserWrapper
